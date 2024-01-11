5836657e14839c5742e9a1800c43a4a62ded59dc
wan <2jin_j@naver.com>
2024-01-11 23:55:24 +0900

    백준 11729 풀이

--- etc/baekjoon/11729.py
+++ etc/baekjoon/11729.py
@@ -1,12 +1,17 @@
-def hanoi_tower(n, start, end) :
-    if n == 1 :
-        print(start, end)
+from sys import stdin
+
+N = int(input())
+
+
+def hanoi(num, start, to, temp):
+    if num == 1:
+        print(start, to)
         return
-       
-    hanoi_tower(n-1, start, 6-start-end) # 1단계
-    print(start, end) # 2단계
-    hanoi_tower(n-1, 6-start-end, end) # 3단계
-    
-n = int(input())
-print(2**n-1)
-hanoi_tower(n, 1, 3)
+
+    hanoi(num - 1, start, temp, to)
+    print(start, to)
+    hanoi(num - 1, temp, to, start)
+
+
+print(2**N - 1)
+hanoi(N, 1, 3, 2)
