5d59942660fc0a6d4576547c52f748854b734498
wan <2jin_j@naver.com>
2024-01-16 22:35:24 +0900

    백준 2343 풀이

---
+++ etc/baekjoon/2343.py
@@ -0,0 +1,45 @@
+# N개의 강의
+# M개의 블루레이
+# 각 블루레이 모두 같은 길이
+
+from sys import stdin
+
+N, M = map(int, stdin.readline().split())
+records = list(map(int, stdin.readline().split()))  # 순서대로, 분 단위
+
+# 블루레이 최소 길이? = M개가 모두 같은 길이
+
+
+# 가장 큰 원소 = start (가장 많은 경우의 수)
+# 모든 원소들의 합 = end (1개만 필요, 가장 작은 경우의 수)
+def size():
+    sum_records = 0
+    cnt = 0
+
+    for record in records:
+        if sum_records + record > mid:
+            cnt += 1
+            sum_records = 0
+
+        sum_records += record
+
+    if sum_records > 0:
+        cnt += 1
+
+    return cnt
+
+
+start, end = max(records), sum(records)
+result = 0
+
+while start <= end:
+    mid = (start + end) // 2
+    count = size()
+
+    if count <= M:
+        result = mid
+        end = mid - 1
+    else:
+        start = mid + 1
+
+print(result)
