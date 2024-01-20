c5cfd85bfa494ff3db89e2e94789b2bf6e32d0d6
wan <2jin_j@naver.com>
2024-01-20 23:12:05 +0900

    백준 14729-2 풀이

--- etc/baekjoon/14729_2.py
+++ etc/baekjoon/14729_2.py
@@ -1,16 +1,55 @@
+# 힙 사용
+
+# 1. 메모리.. 효율적이지 않음
 from sys import stdin
-from heapq import heappop, heappush
+from heapq import heappush, heappop
 
-input = stdin.readline
+N = int(stdin.readline().rstrip())
 
-N = int(input())
-q = [-100] * 7  # inf로 하는게 나을듯
+scores = []
 for _ in range(N):
-    a = float(input())
-    if -q[0] > a:
-        heappop(q)
-        heappush(q, -a)  # 최대 힙, 역순으로 대입
+    n = float(stdin.readline().rstrip())
+    heappush(scores, n)
 
-q.sort(reverse=True)
-for n in q:
-    print("{:.3f}".format(-n))
+for _ in range(7):
+    print("{:.3f}".format(heappop(scores)))
+
+
+# 2. 힙만 사용하지 말고, 로직을 추가하는게 좋음
+from sys import stdin
+from heapq import heappush, heappop
+
+N = int(stdin.readline().rstrip())
+
+scores = [-float("inf")] * 7
+
+for _ in range(N):
+    n = float(stdin.readline().rstrip())
+
+    if scores[0] < -n:  # heap쓴다고 정렬되는건 x, 기본 = 최소 힙
+        heappop(scores)
+        heappush(scores, -n)
+
+scores.sort(reverse=True)  # 마지막에 한번만 정렬
+for s in scores:
+    print("{:.3f}".format(-s))
+
+
+## 근데 아래처럼 sort 써도 속도랑 메모리 사용량은 비슷한듯...
+from sys import stdin
+from heapq import heappush, heappop
+
+N = int(stdin.readline().rstrip())
+
+scores = [float("inf")] * 7
+
+for _ in range(N):
+    n = float(stdin.readline().rstrip())
+
+    if scores[6] > n:
+        scores.pop()
+        heappush(scores, n)
+        scores.sort()
+
+for _ in range(7):
+    print("{:.3f}".format(heappop(scores)))
