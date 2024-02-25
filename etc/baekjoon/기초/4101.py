8c06f85fcb77d8650912efb75b4bd465d8bf0aaf
wan <2jin_j@naver.com>
2024-02-25 23:42:41 +0900

    백준 4101 풀이

---
+++ etc/baekjoon/기초/4101.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    A, B = map(int, input().split())
+    if A == 0 and B == 0:
+        break
+
+    if A > B:
+        print("Yes")
+    else:
+        print("No")
