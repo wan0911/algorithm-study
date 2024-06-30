8203589bee870a5e52a57555fddb60769e14a9c3
wan <2jin_j@naver.com>
2024-06-30 20:11:38 +0900

    백준 1325 풀이 2

--- etc/baekjoon/BFS_DFS/1325_2.py
+++ etc/baekjoon/BFS_DFS/1325_2.py
@@ -1,5 +1,5 @@
 # 해킹
-# dfs로 풀어야 시간복잡도가 작아지나?
+# dfs로 풀어야 시간복잡도가 작아지나? -> ㄴㄴ
 
 import sys
 
@@ -15,5 +15,37 @@ for _ in range(M):
 
 
 # 2. DFS
-def dfs():
-    
+def dfs(v, visited, cnt):
+    visited[v] = 1
+
+    for nv in graph[v]:
+        if not visited[nv]:
+            cnt = dfs(nv, visited, cnt + 1)
+    return cnt
+
+
+res = 0
+lst = []
+for i in range(1, N + 1):
+    visited = [0 for _ in range(N + 1)]
+    tmp = dfs(i, visited, cnt=1)
+
+    if res < tmp:
+        res = tmp
+        lst.clear()
+        lst.append(i)
+    elif res == tmp:
+        lst.append(i)
+
+
+print(*lst)
+
+
+"""
+틀린이유
+
+1. 재귀 함수 -> 메모리 초과 / 시간 초과
+: 초반에 sys.setrecursionlimit(10**6) 을 통해 스택을 위한 array의 사이즈 지정 -> 메모리 초과 오류
+: '스택'으로 구현하면 통과하는데, 스택은 필요 시에 사이즈를 늘리는 dynamic array이기 때문....
+
+"""
