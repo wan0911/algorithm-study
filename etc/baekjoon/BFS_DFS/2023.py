84af02cc995b4813888b295ff5ba10befec27bea
wan <2jin_j@naver.com>
2024-10-09 19:27:48 +0900

    back up

---
+++ etc/baekjoon/BFS_DFS/2023.py
@@ -0,0 +1,20 @@
+# 소수: 1, n
+# N <= 8
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+def isPrime(a):
+    if a % 2 == 0 or a % 3 == 0:
+        return False
+    
+    for j in range(a):
+        
+
+for num in range(10**n, int('9'*n) + 1):
+    for i in range(n):
+        
+        
