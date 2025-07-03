2050050b2caaa5b1ac26ebee66bf0c180bd3b519
wan <2jin_j@naver.com>
2025-07-04 07:53:29 +0900

    해쉬 개념 코드

--- 2025/개념/해쉬/hash.py
+++ 2025/개념/해쉬/hash.py
@@ -8,4 +8,45 @@
 # 충돌 시, idx + 1... 로
 
 
+# 1. dictonary(hash) 구현
+class Dict:
+    def __init__(self):
+        self.items = [None] * 8
 
+    def put(self, key, value):
+        idx = hash(key) % len(self.items)  # hash와 배열 길이 -> idx
+        self.items[idx] = value
+        return
+
+    def get(self, key):
+        idx = hash(key) % len(self.items)
+        return self.items[idx]
+
+
+# 2. 충돌 -> linkedList 활용
+class linkedTuple:
+    def __init__(self):
+        self.items = []  # linkedList 형태로 [[k, v], [k, v], ..] 형태로 구현
+
+    def add(self, key, value):
+        self.append([key, value])
+
+    def get(self, key):
+        for k, v in self.items:
+            if k == key:
+                return v
+
+
+class linkedDict:
+    def __init__(self):
+        self.items = []
+        for _ in range(8):
+            self.items.append(linkedTuple())  # 인자를 linkdList로 할당
+
+    def put(self, key, value):
+        idx = hash(key) % len(self.items)
+        self.items[idx].put(key, value) # 여기 확인
+
+    def get(self, key):
+        idx = hash(key) % len(self.items)
+        return self.items[idx].get(key)
