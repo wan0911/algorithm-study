a3027c347e0c6e590265667324d3ddf7abf1e94e
wan <2jin_j@naver.com>
2025-07-12 23:14:45 +0900

    subset 문제풀이

--- 2025/문제풀이/완전탐색/combinations.py
+++ 2025/문제풀이/완전탐색/combinations.py
@@ -1,23 +1,25 @@
 class Solution(object):
-    def combine(self, n, k):
-        # nCk = 4C2
-        # n이니까 숫자순, n >= k
-        ans = []
+    def subsets(self, nums):
+        ans, c_comb = [], []
 
-        def combinations(start, comb):
-            # depth = r이면 stop
-            if start > n or len(comb) == k:  # n == 1도 OK
+        # depth가 0 ~ len(nums)까지 모두 구해져야 함
+        def combinations(depth, comb, s_idx=0):
+            if len(comb) == depth:
                 ans.append(comb)
-                return  # 리턴 출력할건 없는듯?
+                return
 
-            for j in range(start + 1, n + 1):
-                comb.append(j)
-                # print('chk', comb, ans)
-                combinations(j, comb[:])  # deep copy 중요..
-                comb.pop()  # backtracking
-                # print(comb)
+            # 배열 기준 탐색
+            for idx in range(s_idx, len(nums)):
+                # print(s_idx, idx, nums[idx], ans)
+                c_comb.append(nums[idx])
+                # print(s_idx, idx)
+                combinations(depth, c_comb[:], idx + 1)  # s_idx -> idx로 수정
+                c_comb.pop()
 
-        for i in range(1, n + 1):
-            combinations(i, [i])
+        # 함수 진입점
+        # depth 길이 기준
+        for n in range(len(nums) + 1):
+            combinations(n, [])
 
+        ans.sort()
         return ans
