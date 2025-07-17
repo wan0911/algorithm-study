82d81612192a56627a23f98a29576cd4b6bfa5d4
wan <2jin_j@naver.com>
2025-07-18 07:05:38 +0900

    유효한 괄호쌍 재풀이

--- 2025/문제풀이/완전탐색/permutations_sequence.py
+++ 2025/문제풀이/완전탐색/permutations_sequence.py
@@ -1,22 +1,30 @@
 class Solution(object):
-    def getPermutation(self, n, k):
-        # 주어진 숫자 배열에서 n(k 아님) 길이의 순열을 구하고, k번째 순열을 리턴
-        num_list = [i for i in range(1, n + 1)]
-        result = []
-        def backtrack(ans):
-            if len(result) == k:
-                return
+    def isValid(self, s):
+        # 스택: LIFO -> pop
+        # 맨 위에걸 뽑는다
+        # 그 다음 스택을 확인해서 유효쌍이면 
+        # ({([)]})
+        # [), }, ], )] [
+        # ( -> ) / [ -> ] / { -> }
+        # 빈 스택에 뽑아서 저장.. 닫는 괄호를 만나면 빼기
+        # 일치하지 않는 괄호쌍을 만나면 break
+        pop_list = []
+        par_dict = {"(": ")", "[": "]", "{": "}"}
+        for i in range(len(s)-1, -1, -1):
+            p1 = s[i]
+            if p1 in (')', ']', '}'):
+                pop_list.append(p1)
+            else: # 반대 괄호쌍을 만났을 때
+                if len(pop_list) == 0:
+                    return False
+                if par_dict[p1] == pop_list[-1]:
+                    pop_list.pop()
+                else:
+                    return False      
+        if len(pop_list) == 0:
+            return True
+        else:
+            return False     
 
-            if len(ans) == n:
-                result.append(ans)
-                return
 
-            for num in num_list:
-                if str(num) not in ans:
-                    ans.append(str(num))
-                    backtrack(ans[:])
-                    ans.pop()
-
-        backtrack([])
-        # print(result)
-        return "".join(result[-1])
+        
